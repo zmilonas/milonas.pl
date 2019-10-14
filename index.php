5f82dec5278e7931304d2db0eec637f5b2303d03
zmilonas <zachary@milonas.pl>
2019-10-14 18:47:26 +0200

    initial commit]

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head> 
+<title>Milonas</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta charset="utf-8">
+<meta name="description" content="Milonas w polsce" />
+<link rel="icon" type="image/png" href="icon.png" sizes="192x192">
+<meta name="theme-color" content="#5C6BC0">
+<style>
+@keyframes aa{from{opacity:0;-webkit-transform:translate3d(0,-100%,0);transform:translate3d(0,-100%,0)}to{opacity:1;-webkit-transform:none;transform:none}}
+html,body{margin:0;padding:0;background-color:#5C6BC0;color:#fff;display:flex;height:100%;align-items:center;justify-content:center}h1{font-family:-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;font-size:9vw;font-weight:300;-webkit-animation-name:aa;animation-name:aa;-webkit-animation-duration:1s;animation-duration:1s;-webkit-animation-fill-mode:both;animation-fill-mode:both;}
+</style>
+</head>
+<body>
+	<h1>milonas.pl</h1>
+</body>
